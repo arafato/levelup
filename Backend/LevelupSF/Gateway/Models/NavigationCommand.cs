c5db9b76eea33122b90e9cb60eec7c3559d4348b
olarafat <olarafat@DE-LOANER519>
2016-07-30 01:10:04 +0200

    extended directions

--- Backend/LevelupSF/Gateway/Models/NavigationCommand.cs
+++ Backend/LevelupSF/Gateway/Models/NavigationCommand.cs
@@ -9,9 +9,14 @@ namespace Gateway
     public enum Direction
     {
         NORTH = 10,
+        NORTHEAST = 15,
         EAST = 20,
+        SOUTHEAST = 25,
         SOUTH = 30,
+        SOUTHWEST = 35,
         WEST = 40,
+        NORTHWEST = 45
+
     }
     public class NavigationCommand
     {
