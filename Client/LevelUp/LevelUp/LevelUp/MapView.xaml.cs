f3ea4768ee0cf43eb33e40cc92a0029d5e91294d
maybevs <bmayer@microsoft.com>
2016-07-29 15:29:08 -0700

    Did some more stuff

--- Client/LevelUp/LevelUp/LevelUp/MapView.xaml.cs
+++ Client/LevelUp/LevelUp/LevelUp/MapView.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading;
 using Xamarin.Forms;
+using System.Net.Http;
+using System.Diagnostics;
 
 namespace LevelUp
 {
@@ -13,6 +15,29 @@ namespace LevelUp
         public MapView()
         {
             InitializeComponent();
+
+            
+
+
+        }
+
+        public async Task<string> GetNextWaypoint()
+        {
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetStringAsync($"http://hackclusterup.westeurope.cloudapp.azure.com/api/navigation/GetNextNavigationPoint?latitude=1&longitude=1");
+
+            Device.BeginInvokeOnMainThread(() => {
+                Debug.WriteLine("Hello");
+                }
+            );
+
+            return response;
+        }
+
+        public async void DoSomething(object a, object b)
+        {
+            var response = await GetNextWaypoint();
         }
     }
 }
