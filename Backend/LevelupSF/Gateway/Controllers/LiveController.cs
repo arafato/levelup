3958b2fd761c3184b62f550251037bf2ae4b3390
Andreas Pohl <Annonator@googlemail.com>
2016-07-30 20:02:02 +0200

    Added Azure Storage Packages

---
+++ Backend/LevelupSF/Gateway/Controllers/LiveController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+
+namespace Gateway.Controllers
+{
+    internal class LiveController : ApiController
+    {
+        public void UpdateLocation(Guid routId, float latitude, float longitude)
+        {
+            var storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+        }
+    }
+}
